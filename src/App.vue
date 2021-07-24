c2cd9dba15e8031ba9d837eb0f7a98da883d09f3
BobBy <ayiteybobby@gmail.com>
2021-07-24 18:08:36 +0000

    Setup PWA

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <a
+    style="cursor:pointer"
+    :style="{ display: installBtn }"
+    @click="installer()"
+  >
+    <h1>Install!</h1>
+  </a>
+
+  <HelloWorld msg="Welcome to Your Vue 3 PWA App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+
+  setup() {
+    let installPrompt = null;
+    let installBtn = ref("none");
+
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      installPrompt = e;
+      installBtn.value = "block";
+    });
+
+    let installer = () => {
+      installBtn.value = "none";
+      installPrompt.prompt();
+      installPrompt.userChoice.then((result) => {
+        if (result.outcome === "accepted") {
+          console.log("Install accepted!");
+        } else {
+          console.log("Install denied!");
+        }
+      });
+    };
+
+    return { installer };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +57,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
